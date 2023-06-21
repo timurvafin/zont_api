79dd37a2e20e5cfc33fe3a20b1183b96328b8fda
timurvafin <timur.vafin@flatstack.com>
2023-06-21 16:47:03 +0300

    Format with black

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='zont_api',
-    version='0.1.0',
-    description='Python wrapper for Zont API',
-    author='Timur Vafin',
-    author_email='your.email@example.com',
+    name="zont_api",
+    version="0.1.0",
+    description="Python wrapper for Zont API",
+    author="Timur Vafin",
+    author_email="your.email@example.com",
     packages=find_packages(),
     install_requires=[
-        'aiohttp>=3.8.1',
+        "aiohttp>=3.8.1",
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
--- tests/test_zont_api.py
+++ tests/test_zont_api.py
@@ -6,60 +6,68 @@ from zont_api import ZontAPI
 
 @pytest.fixture
 def api():
-    return ZontAPI('test@example.com', 'my_auth_token', 62953, True)
+    return ZontAPI("test@example.com", "my_auth_token", 62953, True)
 
 
 @pytest.fixture
 def devices_response_json():
-    with open('tests/devices.json') as f:
+    with open("tests/devices.json") as f:
         return json.load(f)
-    
+
 
 @pytest.fixture
 def update_device_response_json():
-    with open('tests/update_device.json') as f:
+    with open("tests/update_device.json") as f:
         return json.load(f)
 
 
 def test_response_without_ok(api, requests_mock, devices_response_json):
-    del devices_response_json['ok'] 
-    requests_mock.post('https://lk.zont-online.ru/api/devices', json=devices_response_json)
-    
+    del devices_response_json["ok"]
+    requests_mock.post(
+        "https://lk.zont-online.ru/api/devices", json=devices_response_json
+    )
+
     with pytest.raises(Exception) as exc_info:
         api.get_device()
-    
+
     assert str(exc_info.value) == 'Response does not contain the "ok" key'
 
 
 def test_response_without_devices(api, requests_mock, devices_response_json):
-    del devices_response_json['devices'] 
-    requests_mock.post('https://lk.zont-online.ru/api/devices', json=devices_response_json)
-    
+    del devices_response_json["devices"]
+    requests_mock.post(
+        "https://lk.zont-online.ru/api/devices", json=devices_response_json
+    )
+
     with pytest.raises(Exception) as exc_info:
         api.get_device()
-    
+
     assert str(exc_info.value) == 'Response does not contain the "devices" key'
 
 
 def test_get_device_not_found(api, requests_mock, devices_response_json):
-    devices_response_json['devices'][0]['id'] = 123456
-    requests_mock.post('https://lk.zont-online.ru/api/devices', json=devices_response_json)
-    
+    devices_response_json["devices"][0]["id"] = 123456
+    requests_mock.post(
+        "https://lk.zont-online.ru/api/devices", json=devices_response_json
+    )
+
     with pytest.raises(Exception) as exc_info:
         api.get_device()
-    
-    assert str(exc_info.value) == 'Device with ID 62953 not found'
+
+    assert str(exc_info.value) == "Device with ID 62953 not found"
 
 
 def test_get_device(api, requests_mock, devices_response_json):
-    requests_mock.post('https://lk.zont-online.ru/api/devices', json=devices_response_json)
+    requests_mock.post(
+        "https://lk.zont-online.ru/api/devices", json=devices_response_json
+    )
 
     expected_device = {
         "id": 62953,
         "ip": "192.168.1.1",
         "is_active": True,
         "name": "Орёл.Котел",
-        "ot_config": [ "ch" ]
+        "ot_config": ["ch"],
     }
 
     result = api.get_device()
@@ -68,43 +76,47 @@ def test_get_device(api, requests_mock, devices_response_json):
 
 
 def test_get_thermometers(api, requests_mock, devices_response_json):
-    requests_mock.post('https://lk.zont-online.ru/api/devices', json=devices_response_json)
+    requests_mock.post(
+        "https://lk.zont-online.ru/api/devices", json=devices_response_json
+    )
 
     expected_thermometers = {
         "Улица": 16.5,
         "Котельная": 34.0,
         "Коллектор подача": 27.7,
-        "Спальня": 24.8
+        "Спальня": 24.8,
     }
 
     result = api.get_thermometers()
 
     assert result == expected_thermometers
 
-def test_update_device(api, requests_mock, update_device_response_json):
-    requests_mock.post('https://lk.zont-online.ru/api/update_device', json=update_device_response_json)
 
-    result = api.update_device({
-        "ot_config": [
-            "ch",
-            "dhw"
-        ]
-    })
-    
+def test_update_device(api, requests_mock, update_device_response_json):
+    requests_mock.post(
+        "https://lk.zont-online.ru/api/update_device", json=update_device_response_json
+    )
+
+    result = api.update_device({"ot_config": ["ch", "dhw"]})
+
     assert result == update_device_response_json
 
 
 def test_enable_dhw(api, requests_mock, update_device_response_json):
-    requests_mock.post('https://lk.zont-online.ru/api/update_device', json=update_device_response_json)
+    requests_mock.post(
+        "https://lk.zont-online.ru/api/update_device", json=update_device_response_json
+    )
 
     result = api.enable_dhw()
-    
+
     assert result == update_device_response_json
 
 
 def test_disable_dhw(api, requests_mock, update_device_response_json):
-    requests_mock.post('https://lk.zont-online.ru/api/update_device', json=update_device_response_json)
+    requests_mock.post(
+        "https://lk.zont-online.ru/api/update_device", json=update_device_response_json
+    )
 
     result = api.disable_dhw()
-    
+
     assert result == update_device_response_json
